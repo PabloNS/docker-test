fae14a2db0f5d1f85d5f6c37b4726c915eb3def5
pablonunezserra <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>
2021-10-27 18:37:07 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# docker-test
+RUN sh start.sh
+
+This will build 2 applications, deploy 2 docker containers
+
+http://localhost:8080
+
+http://localhost:8081
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./mvnw clean install
+cd service1
+docker build -t service1 .
+cd ..
+cd service2
+docker build -t service2 .
+cd ..
+docker compose up -d
