3023af8738c0391ea0ea458f02eeb78b90576028
pablonunezserra <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>
2021-11-04 10:38:33 +0100

    Move to service

--- service1/src/main/java/pablonuserra/dockertest/service1/controller/Service1Controller.java
+++ service1/src/main/java/pablonuserra/dockertest/service1/controller/Service1Controller.java
@@ -1,52 +1,27 @@
 package pablonuserra.dockertest.service1.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pablonuserra.dockertest.service1.model.PostgresEntity;
-import pablonuserra.dockertest.service1.model.PostgresEntityRepository;
 import pablonuserra.dockertest.service1.service.Service1Service;
 
 @RestController
-@Slf4j
 public class Service1Controller {
-
-    Logger logger = LoggerFactory.getLogger(Service1Controller.class);
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-
-    @Autowired
-    private PostgresEntityRepository entity1Repository;
 
     @Autowired
     private Service1Service service1Service;
 
-    @Value("${kafka.topic.name}")
-    private String topicName;
-
-    int messagesCount = 0;
-
-    int entityCount = 0;
-
-    @GetMapping("/sendMessage")
+    @PostMapping("/sendMessage")
     public String sendMessage(){
-        kafkaTemplate.send(topicName, "Message sent from service1 #" + ++messagesCount);
-        return "Message sent!";
+        return service1Service.sendMessage();
     }
 
     @PostMapping("/entity")
     public PostgresEntity createEntity(){
-        PostgresEntity entity1 = PostgresEntity.builder().name("Entity" + ++entityCount).build();
-        entity1Repository.save(entity1);
-        return entity1;
+        return service1Service.createEntity();
     }
 
     @GetMapping("/entity/{id}")
--- service1/src/main/java/pablonuserra/dockertest/service1/service/Service1Service.java
+++ service1/src/main/java/pablonuserra/dockertest/service1/service/Service1Service.java
@@ -1,13 +1,17 @@
 package pablonuserra.dockertest.service1.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import pablonuserra.dockertest.service1.model.PostgresEntity;
 import pablonuserra.dockertest.service1.model.PostgresEntityRepository;
 
 @Service
+@Slf4j
 public class Service1Service {
 
     Logger logger = LoggerFactory.getLogger(Service1Service.class);
@@ -15,9 +19,30 @@ public class Service1Service {
     @Autowired
     private PostgresEntityRepository entity1Repository;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${kafka.topic.name}")
+    private String topicName;
+
+    int messagesCount = 0;
+
+    int entityCount = 0;
+
+    public String sendMessage(){
+        kafkaTemplate.send(topicName, "Message sent from service1 #" + ++messagesCount);
+        return "Message sent!";
+    }
+
     public PostgresEntity getEntity(Long id){
         return entity1Repository.findById(id).orElseGet(() -> {
             logger.error("Entity1 with id {} doesn't exist", id);
             return null; });
+    }
+
+    public PostgresEntity createEntity(){
+        PostgresEntity entity1 = PostgresEntity.builder().name("Entity" + ++entityCount).build();
+        entity1Repository.save(entity1);
+        return entity1;
     }
 }
